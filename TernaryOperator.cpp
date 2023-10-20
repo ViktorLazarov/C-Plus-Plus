b6fa5016c18da070066e71a76626ad28688efc72
Viktor Lazarov <viktorlazarov@abv.bg>
2023-10-20 14:59:48 +0200

    learned about ternary operator ?:

---
+++ TernaryOperator.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+int main(){
+
+    // ternary operator ?: = replacement to an if/else statement
+    //condition ? expression1 : expression2
+
+
+    int grade = 55;
+
+    grade > 60 ? cout << "Grade is greater then 60" : cout << "Grade is lower then 60";
+
+
+
+
+    return 0;
+}

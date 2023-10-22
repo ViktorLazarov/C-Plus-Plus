93ac1158dc8901f2cd8f3e9b1a326b074336bfba
Viktor Lazarov <viktorlazarov@abv.bg>
2023-10-22 11:18:17 +0200

    separated code into 2 functions

--- DecimalToBinary.cpp
+++ DecimalToBinary.cpp
@@ -2,20 +2,27 @@
 #include <cmath>
 
 using namespace std;
-
-int main()
-{
     int input;
     string binary;
 
+void getInput(){
     cout << "Enter a decimal number!: ";
     cin >> input;
+}
 
+void calculate(){
     while (input > 0)
     {
         binary.insert(0, to_string(input % 2));
         input /= 2;
     }
     cout << binary;
+}
+
+int main()
+{
+    getInput();
+    calculate();
+
     return 0;
 }

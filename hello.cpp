e7085aede2840b272a92d3e8569d533e095b2d49
Viktor Lazarov <viktorlazarov@abv.bg>
2023-11-20 09:56:50 +0100

    sdfg

--- hello.cpp
+++ hello.cpp
@@ -2,18 +2,22 @@
 #include <iomanip>
 #include <string>
 using namespace std;
+string test();
+int main()
+{
 
-int main() {
-    
     std::cout << "Hello World!\n";
-    for (int i = 0; true ; i++)
-    {
-        cout << i << '\n';
-        if (i == 25)
-        {
-            break;
-        }
-        
-    }
-    
+   
+    cout << test();
+}
+
+string test()
+{
+    cout << 1 << '\n';
+    cout << 2 << '\n';
+    cout << 3 << '\n';
+    return "Hello";
+    cout << 4 << '\n';
+    cout << 5 << '\n';
+    cout << 6 << '\n';
 }

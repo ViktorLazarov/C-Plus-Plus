9cc187245305d28a4013532510243879f44cf13e
Viktor Lazarov <viktorlazarov@abv.bg>
2023-11-23 18:42:36 +0100

    ghj

--- hello.cpp
+++ hello.cpp
@@ -1,9 +1,29 @@
 #include <iostream>
 #include <iomanip>
 #include <string>
-// using namespace std;
+using namespace std;
 
-int main() {
-    
-    std::cout << "Hello World!";
+int main()
+{
+
+    cout << "Hello World!\n";
+    string userInput;
+    int a;
+    getline(cin, userInput);
+    if (userInput.empty())
+    {
+        cout << "User pressed only Enter";
+    }
+    else
+    {
+        stringstream ss(userInput);
+        if (ss >> a)
+        {
+            cout << "User entered an integer";
+        }
+        else
+        {
+            cout << "user entered something else";
+        }
+    }
 }

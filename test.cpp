c50003cd2ae9cc4a631c40ab0a1988a0d2592b9f
Viktor Lazarov <viktorlazarov@abv.bg>
2023-12-04 18:37:43 +0100

    asd

--- test.cpp
+++ test.cpp
@@ -7,19 +7,32 @@ int main()
 {
     // passing an array as an argument
     int arr[5];
- 
-    fill(arr, 5);
-    for (int i = 0; i < 5; i++)
-    {
-        std::cout << arr[i] << '\n';
-    }
+
+    // fill(arr, 5);
+    // for (int i = 0; i < 5; i++)
+    // {
+    //     std::cout << arr[i] << '\n';
+    // }
     std::cout << "***********************************" << '\n';
     // passing ints by reference
-    int x = 10;
-    int y = 5;
-    changeInt(x, y);
+    int x = 9;
+    int y = 7;
+    // changeInt(x, y);
     std::cout << x << '\n';
     std::cout << y << '\n';
+
+    std::string map(x * y, '-');
+    for (int i = 0; i < x; i++)
+    {
+        for (int j = 0; j < y; j++)
+        {
+            std::cout << map[j];
+        }
+        std::cout << '\n';
+    }
+    map[23] = 'A';
+
+    std::cout << map;
 
     return 0;
 }

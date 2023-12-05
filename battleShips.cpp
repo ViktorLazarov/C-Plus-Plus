fa3f3ad3b8b1ff8b8ca7d8925de28d7417c75870
Viktor Lazarov <viktorlazarov@abv.bg>
2023-12-05 14:53:53 +0100

    asd

--- battleShips.cpp
+++ battleShips.cpp
@@ -1,11 +1,18 @@
 #include <iostream>
+#include <ctime>
+#include <vector>
+#include <array>
 
 using namespace std;
 
 void printMap(string map, int rows, int cols);
-void changeElement(string &map, int cols, int userRow, int userCol);
+// void changeElement(string &map, int cols, int userRow, int userCol);
+
 int main()
 {
+    int foundShips = 0;
+    int numberOfShips = 4;
+    // define dimentions of the map
     int rows;
     int cols;
 
@@ -14,26 +21,66 @@ int main()
     cout << "Map cols: ";
     cin >> cols;
 
-    int userRow;
-    int userCol;
-    cout << "Change an element\n";
-    cout << "Row: ";
-    cin >> userRow;
-    cout << "Col: ";
-    cin >> userCol;
-
+    // create the map
     string map(rows * cols, '-');
 
-    changeElement(map, cols, userRow, userCol);
+    // randomly generate the position of the ships
+    // int ships[numberOfShips][2];
+    vector<array<int, 2>> ships;
+    int shipRow;
+    int shipCol;
+    srand(time(0));
+    for (int i = 0; i < numberOfShips; i++)
+    {
+        // generate random values for the position of every ship
+        shipRow = (rand() % rows) + 1;
+        shipCol = (rand() % cols) + 1;
+        ships.push_back({{shipRow, shipCol}});
+    }
+
+    for (int i = 0; i < numberOfShips; i++)
+    {
+        cout << ships[i][0] << ' ' << ships[i][1] << endl;
+    }
+
+    // get user guess
+    int userRow;
+    int userCol;
+
+    while (foundShips < 4)
+    {
+
+        for (int i = (rand() % ships.size()) + 1; ships.size() > 0;)
+        {
+            cout << "Guess where the ship is!\n";
+            cout << "Row: ";
+            cin >> userRow;
+            cout << "Col: ";
+            cin >> userCol;
+            if (userRow == ships[i][0] && userCol == ships[i][1])
+            {
+                cout << "You have found a ship!\n";
+                foundShips++;
+                ships.erase(ships.begin() + i);
+            }
+        }
+        if (foundShips == 4)
+        {
+            cout << "Congratulations! You have found all the hidden ships!\n";
+        }
+    }
+
+    // changeElement(map, cols, userRow, userCol);
     printMap(map, rows, cols);
 
-        return 0;
+    return 0;
 }
-void changeElement(string &map, int cols, int userRow, int userCol)
-{
 
-    map[((userRow - 1) * cols) + (userCol-1)] = 'X';
-}
+// void changeElement(string &map, int cols, int userRow, int userCol)
+// {
+
+//     map[((userRow - 1) * cols) + (userCol-1)] = 'X';
+// }
 
 void printMap(string map, int rows, int cols)
 {
--- vectorTest.cpp
+++ vectorTest.cpp
@@ -1,36 +1,61 @@
+// #include <iostream>
+// #include <vector>
+
+// using namespace std;
+// int main()
+// {
+//     vector<vector<string>> userDetails;
+
+//     vector<string> nameAndPass;
+
+//     while (true)
+//     {
+//         string name;
+//         string pass;
+
+//         cout << "Enter Name: \n";
+//         cin >> name;
+//         if (name == "exit")
+//         {
+//             break;
+//         }
+//         cout << "Enter Pass: \n";
+//         cin >> pass;
+
+//         nameAndPass.push_back(name);
+//         nameAndPass.push_back(pass);
+
+//         userDetails.push_back(nameAndPass);
+//         nameAndPass.clear();
+//         }
+
+//     cout << "Name: " << userDetails.at(1).at(0) << endl;
+//     cout << "Pass: " << userDetails.at(1).at(1) << endl;
+
+//     return 0;
+// }
 #include <iostream>
 #include <vector>
 
-using namespace std;
-int main()
-{
-    vector<vector<string>> userDetails;
+int main() {
+    std::vector<int> vec = {1, 2, 3, 4, 5};
 
-    vector<string> nameAndPass;
-
-    while (true)
+    // Removing the element at index 2
+    for (int i = 0; i < vec.size(); i++)
     {
-        string name;
-        string pass;
-
-        cout << "Enter Name: \n";
-        cin >> name;
-        if (name == "exit")
+        if (vec[i] == 1)
         {
-            break;
+            vec.erase(vec.begin()+i);
         }
-        cout << "Enter Pass: \n";
-        cin >> pass;
+        
+    }
+    
 
-        nameAndPass.push_back(name);
-        nameAndPass.push_back(pass);
-
-        userDetails.push_back(nameAndPass);
-        nameAndPass.clear();
-        }
-
-    cout << "Name: " << userDetails.at(1).at(0) << endl;
-    cout << "Pass: " << userDetails.at(1).at(1) << endl;
+    // Displaying the modified vector
+    for (int num : vec) {
+        std::cout << num << " ";
+    }
+    std::cout << std::endl;
 
     return 0;
 }

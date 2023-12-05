072ac83b00b786b744b83a7777eaeb806af1fa83
Viktor Lazarov <viktorlazarov@abv.bg>
2023-12-05 17:56:26 +0100

    removed vector code

--- battleShips.cpp
+++ battleShips.cpp
@@ -1,7 +1,5 @@
 #include <iostream>
 #include <ctime>
-#include <vector>
-#include <array>
 
 using namespace std;
 
@@ -12,6 +10,7 @@ int main()
 {
     int foundShips = 0;
     int numberOfShips = 4;
+
     // define dimentions of the map
     int rows;
     int cols;
@@ -25,8 +24,8 @@ int main()
     string map(rows * cols, '-');
 
     // randomly generate the position of the ships
-    // int ships[numberOfShips][2];
-    vector<array<int, 2>> ships;
+    int ships[numberOfShips][2];
+    
     int shipRow;
     int shipCol;
     srand(time(0));
@@ -35,7 +34,7 @@ int main()
         // generate random values for the position of every ship
         shipRow = (rand() % rows) + 1;
         shipCol = (rand() % cols) + 1;
-        ships.push_back({{shipRow, shipCol}});
+        
     }
 
     for (int i = 0; i < numberOfShips; i++)
@@ -46,24 +45,14 @@ int main()
     // get user guess
     int userRow;
     int userCol;
+    cout << "Guess where the ship is!\n";
+    cout << "Row: ";
+    cin >> userRow;
+    cout << "Col: ";
+    cin >> userCol;
 
     while (foundShips < 4)
     {
-
-        for (int i = (rand() % ships.size()) + 1; ships.size() > 0;)
-        {
-            cout << "Guess where the ship is!\n";
-            cout << "Row: ";
-            cin >> userRow;
-            cout << "Col: ";
-            cin >> userCol;
-            if (userRow == ships[i][0] && userCol == ships[i][1])
-            {
-                cout << "You have found a ship!\n";
-                foundShips++;
-                ships.erase(ships.begin() + i);
-            }
-        }
         if (foundShips == 4)
         {
             cout << "Congratulations! You have found all the hidden ships!\n";

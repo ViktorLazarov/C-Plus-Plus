23fbf55833e0f1232c9ec0ca2e3d1cdae93a76d1
Viktor Lazarov <viktorlazarov@abv.bg>
2024-01-09 11:17:51 +0100

    optimized output

--- DecimalToBinary.cpp
+++ DecimalToBinary.cpp
@@ -16,13 +16,14 @@ void calculate()
     for (int i = 0; i < input.size(); i++)
     {
         char character = input[i];
-        cout << character << '\n';
+        cout << character << " > " << (int)character << '\n';
         while (character > 0)
         {
             binary.insert(0, to_string(character % 2));
             character /= 2;
         }
         cout << binary << '\n';
+        cout << '\n';
         binary.clear();
     }
 }
